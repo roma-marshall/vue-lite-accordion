7a6aa8905d62d8e53b6bffb1547bb5366aec5ba5
roma_marshall <roma_marshall@outlook.com>
2022-11-18 16:06:26 +0100

    setup commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <roma-accordion></roma-accordion>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RomaAccordion from './components/RomaAccordion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RomaAccordion
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/RomaAccordion.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  
+</div>
+</template>
+
+<script>
+export default {
+  name: 'RomaAccordion',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
